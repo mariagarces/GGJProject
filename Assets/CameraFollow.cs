e52314e9c44911e6467c437d1b4cc9ae1df0cb3a
mafe garces <mafegarces@hotmail.com>
2021-01-31 11:47:16 -0500

    update camera

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -7,6 +7,9 @@ public class CameraFollow : MonoBehaviour
     public GameObject follow1;
     public GameObject follow2;
     public Vector2 minCamPos, maxCamPos;
+    public float smoothTime;
+
+    private Vector2 velocity;
 
     // Start is called before the first frame update
     void Start()
@@ -17,10 +20,10 @@ public class CameraFollow : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        float posX1 = follow1.transform.position.x;
-        float posY1 = follow1.transform.position.y;
-        float posX2 = follow2.transform.position.x;
-        float posY2 = follow2.transform.position.y;
+        float posX1 = Mathf.SmoothDamp(transform.position.x, follow1.transform.position.x, ref velocity.x, smoothTime);
+        float posY1 = Mathf.SmoothDamp(transform.position.y, follow1.transform.position.y, ref velocity.y, smoothTime);
+        float posX2 = Mathf.SmoothDamp(transform.position.x, follow2.transform.position.x, ref velocity.x, smoothTime);
+        float posY2 = Mathf.SmoothDamp(transform.position.y, follow2.transform.position.y, ref velocity.y, smoothTime);
 
         if (posX1 < posX2)
         {
@@ -35,6 +38,6 @@ public class CameraFollow : MonoBehaviour
             Mathf.Clamp(posX1, minCamPos.x, maxCamPos.x),
             Mathf.Clamp(posY1, minCamPos.y, maxCamPos.y),
             transform.position.z);
-        }       
+        }
     }
 }
